1ef1a4b70ab03bade2c720388e67e69b818af647
rora0320 <83759908+rora0320@users.noreply.github.com>
2023-08-24 16:30:50 +0900

    atomwithStorage로 저장된 값이 로컬스토리지 대신 쓰일수 있는지 테스트

--- src/utils/requestMethod.js
+++ src/utils/requestMethod.js
@@ -1,17 +1,19 @@
 import axios from 'axios';
+import {useAtomValue} from 'jotai';
+import {TokenAtom} from '../jotai/jotai';
 
-axios.defaults.baseURL = 'http://192.168.20.16:3000'
+axios.defaults.baseURL = '';
 const unAuthClient = axios.create({
-    baseURL: '192.168.20.16:3000',
+    baseURL: 'http://localhost:3001',
 });
 const authClient = axios.create({
-    baseURL: '192.168.20.16:3000',
+    baseURL: 'http://localhost:3001',
 });
 
 authClient.interceptors.request.use(
     function (config) {
-        const token = localStorage.getItem('login_T');
-
+        // const token = localStorage.getItem('login_board');
+        const token = useAtomValue(TokenAtom);
         // if (!token) throw new Error("localStorage is empty", { statusCode: 401 });
         if (!token) {
             const error = new Error('localStorage is empty');
@@ -21,6 +23,7 @@ authClient.interceptors.request.use(
 
         // 토큰 있으면 헤더로 백엔드에 보내
         if (token) {
+            console.log('토큰보내??', token)
             if (config.headers) config.headers.Authorization = `Bearer ${token}`;
             return config;
         } else {
@@ -29,8 +32,8 @@ authClient.interceptors.request.use(
     },
     function (error) {
         if (error.response?.status === 401 || error.code === 401) {
-            window.localStorage.removeItem('login_T');
-            window.localStorage.removeItem('userData');
+            window.localStorage.removeItem('login_board');
+            window.localStorage.removeItem('form');
             window.location.href = '/';
         }
         return Promise.reject(error);
@@ -40,13 +43,14 @@ authClient.interceptors.request.use(
 // if response is 401, remove token and redirect to login page
 authClient.interceptors.response.use(
     function (response) {
+        console.log('응답???', response)
         return response;
     },
     function (error) {
         if (error.response?.status === 401 || error.code === 401) {
             // dispatch({ type: LOGOUT });
-            window.localStorage.removeItem('login_T');
-            window.localStorage.removeItem('userData');
+            window.localStorage.removeItem('login_board');
+            window.localStorage.removeItem('form');
             window.location.href = '/';
         }
         return Promise.reject(error);
