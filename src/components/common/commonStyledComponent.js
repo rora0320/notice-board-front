97744539a05afd8ab3ba721947419caf07aa4210
rora0320 <83759908+rora0320@users.noreply.github.com>
2023-08-24 17:05:37 +0900

    공통 컴포넌트 밖으로 빼서 같이 쓰기

--- src/components/common/commonStyledComponent.js
+++ src/components/common/commonStyledComponent.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+
 export const WrapForm = styled.div`
   display: flex;
   align-items: center;
@@ -17,5 +18,48 @@ export const WrapForm = styled.div`
       color: #e4e4e4;
       border-bottom: 1.5px solid #9e9e9e;
     }
+  }
+`;
+
+export const ModalBackGround = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background: rgba(0, 0, 0, 0.8);
+  z-index: 2;
+`;
+
+export const ModalWrap = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  width: 500px;
+  padding: 20px;
+  border-radius: 5px;
+  background-color: #313131;
+  color: #e4e4e4;
+
+  z-index: 1;
+`;
+
+export const TitleWrap = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: flex-end;
+  margin-bottom: 20px;
+  font-size: 25px;
+
+  .titleHead {
+    position: absolute;
+    left: 37%;
+    text-align: center;
+  }
+
+  svg:hover {
+    cursor: Pointer;
   }
 `;
--- src/components/common/joinUsModal/JoinUsModal.js
+++ src/components/common/joinUsModal/JoinUsModal.js
@@ -1,148 +1,108 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components';
-import { TfiClose } from 'react-icons/tfi';
-import { Button, TextField } from '@mui/material';
+import {TfiClose} from 'react-icons/tfi';
+import {Button, TextField} from '@mui/material';
 import _ from 'lodash';
-import { unAuthClient } from '../../../utils/requestMethod';
+import {unAuthClient} from '../../../utils/requestMethod';
+import {ModalBackGround, ModalWrap, TitleWrap} from '../commonStyledComponent';
 
-const JoinUsModal = ({ openModalJoinUs }) => {
-  const emailRegex = /^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
-  const [idEmail, setIdEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [fullName, setFullName] = useState('');
+const JoinUsModal = ({openModalJoinUs}) => {
+    const emailRegex = /^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
+    const [idEmail, setIdEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [fullName, setFullName] = useState('');
 
-  const handleInputForm = (event) => {
-    switch (event.target.name) {
-      case 'ID':
-        if (!emailRegex.test(event.target.value)) return;
-        setIdEmail(event.target.value);
-        break;
-      case 'PASSWORD':
-        setPassword(event.target.value);
-        break;
-      case 'NAME':
-        setFullName(event.target.value);
-        break;
-    }
-  };
-  const checkedForm = () => {
-    if (_.isEmpty(idEmail) || _.isEmpty(password) || _.isEmpty(fullName)) {
-      return false;
-    } else {
-      return true;
-    }
-  };
-  const submitInputForm = async () => {
-    const body = {
-      id: idEmail,
-      password: password,
-      name: fullName,
+    const handleInputForm = (event) => {
+        switch (event.target.name) {
+            case 'ID':
+                if (!emailRegex.test(event.target.value)) return;
+                setIdEmail(event.target.value);
+                break;
+            case 'PASSWORD':
+                setPassword(event.target.value);
+                break;
+            case 'NAME':
+                setFullName(event.target.value);
+                break;
+        }
     };
-    if (checkedForm()) {
-      try {
-        const { data } = unAuthClient.post(
-          '192.168.20.16:3000/user/signup',
-          body
-        );
-        console.log('data', data);
-      } catch (e) {
-        console.log('사용자 추가 에러', e);
-      }
-    }
-  };
-  return (
-    <ModalBackGround>
-      <ModalWrap>
-        <TitleWrap>
-          <h3 className="titleHead">사용자 추가</h3>
-          <TfiClose className="titleClose" onClick={openModalJoinUs} />
-        </TitleWrap>
-        <ContentWrap>
-          <InputFormWrap>
-            <p>ID :</p>
-            <TextField
-              name="ID"
-              className="inputs_textfield"
-              placeholder="Email Address"
-              variant="standard"
-              value={idEmail}
-              onChange={handleInputForm}
-            />
-          </InputFormWrap>
-          <InputFormWrap>
-            <p>Password :</p>
-            <TextField
-              name="PASSWORD"
-              className="inputs_textfield"
-              placeholder="Password"
-              variant="standard"
-              value={password}
-              onChange={handleInputForm}
-            />
-          </InputFormWrap>
-          <InputFormWrap>
-            <p>Name :</p>
-            <TextField
-              name="NAME"
-              className="inputs_textfield"
-              placeholder="Name"
-              variant="standard"
-              value={fullName}
-              onChange={handleInputForm}
-            />
-          </InputFormWrap>
-          <CustomBtnWrap>
-            <CustomBtn variant="contained" onClick={submitInputForm}>
-              저장
-            </CustomBtn>
-          </CustomBtnWrap>
-        </ContentWrap>
-      </ModalWrap>
-    </ModalBackGround>
-  );
+    const checkedForm = () => {
+        if (_.isEmpty(idEmail) || _.isEmpty(password) || _.isEmpty(fullName)) {
+            return false;
+        } else {
+            return true;
+        }
+    };
+    const submitInputForm = async () => {
+        const body = {
+            id: idEmail,
+            password: password,
+            name: fullName,
+        };
+        if (checkedForm()) {
+            try {
+                const {data} = unAuthClient.post(
+                    '192.168.20.16:3000/user/signup',
+                    body
+                );
+                console.log('data', data);
+            } catch (e) {
+                console.log('사용자 추가 에러', e);
+            }
+        }
+    };
+    return (
+        <ModalBackGround>
+            <ModalWrap>
+                <TitleWrap>
+                    <h3 className="titleHead">사용자 추가</h3>
+                    <TfiClose className="titleClose" onClick={openModalJoinUs}/>
+                </TitleWrap>
+                <ContentWrap>
+                    <InputFormWrap>
+                        <p>ID :</p>
+                        <TextField
+                            name="ID"
+                            className="inputs_textfield"
+                            placeholder="Email Address"
+                            variant="standard"
+                            value={idEmail}
+                            onChange={handleInputForm}
+                        />
+                    </InputFormWrap>
+                    <InputFormWrap>
+                        <p>Password :</p>
+                        <TextField
+                            name="PASSWORD"
+                            className="inputs_textfield"
+                            placeholder="Password"
+                            variant="standard"
+                            value={password}
+                            onChange={handleInputForm}
+                        />
+                    </InputFormWrap>
+                    <InputFormWrap>
+                        <p>Name :</p>
+                        <TextField
+                            name="NAME"
+                            className="inputs_textfield"
+                            placeholder="Name"
+                            variant="standard"
+                            value={fullName}
+                            onChange={handleInputForm}
+                        />
+                    </InputFormWrap>
+                    <CustomBtnWrap>
+                        <CustomBtn variant="contained" onClick={submitInputForm}>
+                            저장
+                        </CustomBtn>
+                    </CustomBtnWrap>
+                </ContentWrap>
+            </ModalWrap>
+        </ModalBackGround>
+    );
 };
 
-const ModalBackGround = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  bottom: 0;
-  right: 0;
-  background: rgba(0, 0, 0, 0.8);
-  z-index: 2;
-`;
-
-const ModalWrap = styled.div`
-  position: fixed;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-
-  width: 500px;
-  padding: 20px;
-  border-radius: 5px;
-  background-color: #313131;
-  color: #e4e4e4;
-
-  z-index: 1;
-`;
-const TitleWrap = styled.div`
-  position: relative;
-  display: flex;
-  justify-content: flex-end;
-  margin-bottom: 20px;
-  font-size: 25px;
-
-  .titleHead {
-    position: absolute;
-    left: 37%;
-    text-align: center;
-  }
-
-  svg:hover {
-    cursor: Pointer;
-  }
-`;
 const ContentWrap = styled.div`
   display: flex;
   flex-direction: column;
